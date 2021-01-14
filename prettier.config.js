b8c96fd81de40970440a220fa0a399f9f7d46c6b
Eymard Neto <eymardneto115@gmail.com>
2021-01-13 23:40:00 -0300

    Projeto iniciado com prettier, eslint, express, typescript e comandos de serve:dev configurados

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  singleQuote: true, // áspas simples
+  trailingComma: 'all', // Virgula no final de objetos
+  arrowParens: 'avoid', // Não colocar parenteses em arrow functions com 1 parâmetro
+};
+
